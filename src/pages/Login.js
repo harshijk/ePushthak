5ec521bcbbe6b851acdcaeb1ecbff4bf911bec47
HARSHITH <harshithjk071@gmail.com>
2024-02-17 22:41:22 +0530

    Login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -30,6 +30,25 @@ export const Login = () => {
 
   }
 
+  async function handleGuest(event){
+    event.preventDefault();
+    const authDetail ={
+      email : process.env.REACT_APP_GUESTEMAIL,
+      password : process.env.REACT_APP_GPASSWORD
+    }
+    try{
+      const data = await login(authDetail);
+    data.accessToken ? navigate('/products'): toast.error(data)
+    }catch(error){
+      toast.error(error.message,{closeButton:true , position :"bottom-center",
+      autoClose:5000,
+      closeOnClick : true})
+    }
+
+    }
+
+  
+
 
     
 
@@ -49,7 +68,7 @@ export const Login = () => {
             </div>
             <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Log In</button>
           </form>
-          {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+          <button onClick={handleGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
       </main>
     )
   }
